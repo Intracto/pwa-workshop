c9dea0491e52fc2b98013261897ee40622aaa11c
Sead Memic <sead.memic@intracto.com>
2019-01-31 11:23:59 +0100

    module 1, assignment fetch added

---
+++ module1/fetch/app/js/main.js
@@ -0,0 +1,31 @@
+// Define jsonButton from the DOM
+const jsonButton = document.getElementById('json-btn');
+
+// Event listener for click to start fetching
+jsonButton.addEventListener('click', () => {
+
+  // 1: Fetch data from example.json [TODO]
+  fetch()
+
+  // 2: Validate response on OK, otherwise throw error [TODO]
+  .then((response) => {
+
+  })
+
+  // 3: Return response as JSON [TODO]
+  .then((response) => {
+
+  })
+
+  // 4: Log result to console [TODO]
+  .then((result) => {
+
+  })
+
+  // 5: Catch errors and log them [TODO]
+  .catch((error) => {
+
+  });
+
+});
+
---
+++ module1/fetch/solution/js/main.js
@@ -0,0 +1,34 @@
+// Define jsonButton from the DOM
+const jsonButton = document.getElementById('json-btn');
+
+// Event listener for click to start fetching
+jsonButton.addEventListener('click', () => {
+
+  // Fetch data from example.json
+  fetch('data/example.json')
+
+  // Validate response on OK, otherwise throw error
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  })
+
+  // Return response as JSON
+  .then((response) => {
+    return response.json();
+  })
+
+  // Log result to console
+  .then((result) => {
+    console.log(result);
+  })
+
+  // Catch errors and log them
+  .catch((error) => {
+    console.log('Looks like there was a problem:', error);
+  });
+
+});
+
